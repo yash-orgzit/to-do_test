97c811178d836e8b4ce56f9878817f37acc6befa
Yash Goyal <yash@orgzit.com>
2024-01-22 18:19:37 +0530

    test Commit

---
+++ components/Notes.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Notes = props => {
+    return(
+        <View>
+            <Text>Notes</Text>
+        </View>
+    );
+};
+
+export default Notes;
---
+++ components/login.js
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const Login = props => {
+  return <View></View>;
+};
+
+export default Login;
